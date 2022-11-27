295ba2b41a7cec6ba892afe425838a45405014ec
Osa <harisosa.93@gmail.com>
2022-11-27 13:09:55 +0700

    category-preview , catrgory in route, introduce style component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,29 +7,29 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import './navigation.styles.scss';
 import { CartContext } from "../../contexts/cart.context";
+import { NavigationContainer , NavLinks,NavLink,LogoContainer } from "./navigation.styles";
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext)
     const { isCartOpen } = useContext(CartContext)
     return (
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to='/'>
+            <NavigationContainer className="navigation">
+                <LogoContainer to='/'>
                     <CrwnLogo className='logo' />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to='/shop'>SHOP</Link>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to='/shop'>SHOP</NavLink>
                     {
                         currentUser ? (
-                            <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
-                        ) : <Link className="nav-link" to='/auth'>SIGN IN</Link>
+                            <NavLink as='span' className='nav-link' onClick={signOutUser}>SIGN OUT</NavLink>
+                        ) : <NavLink to='/auth'>SIGN IN</NavLink>
                     }
                 <CartIcon />
-                </div>
+                </NavLinks>
                 {isCartOpen && <CartDropdown />}
-            </div>
+            </NavigationContainer>
             <Outlet />
         </Fragment>
     )
